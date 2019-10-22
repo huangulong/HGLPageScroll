59c681b7bba35e7f48d72216193ee28a1b9c4be6
huanggulong <2568061134@qq.com>
2019-10-22 12:32:52 +0800

    temp

---
+++ HGLPageScroll/HGLPageScroll.h
@@ -0,0 +1,19 @@
+//
+//  HGLPageScroll.h
+//  HGLPageScroll
+//
+//  Created by admin on 2019/10/22.
+//  Copyright © 2019 历山大亚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HGLPageScroll.
+FOUNDATION_EXPORT double HGLPageScrollVersionNumber;
+
+//! Project version string for HGLPageScroll.
+FOUNDATION_EXPORT const unsigned char HGLPageScrollVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HGLPageScroll/PublicHeader.h>
+
+
